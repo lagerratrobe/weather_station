99295917f0938e357aef547fdf837d1fb039556e
randre <randre@gmail.com>
2022-08-19 13:20:26 -0700

    Adds Montreal, Albany and Stroud stations

--- R/UpdateStationObs.R
+++ R/UpdateStationObs.R
@@ -13,7 +13,7 @@ source("/home/randre/Code/weather_station/R/GetWeatherData.R")
 data_obs <- readRDS("/home/randre/Code/weather_station/Data/station_obs.RDS")
 
 # Stations to pull data for
-stations <- c('KTXDALLA724', 'KWASEATT2743', 'KWASEQUI431')
+stations <- c('KTXDALLA724', 'KWASEATT2743', 'KWASEQUI431', 'ISTROU34', 'IWESTMOU2', 'KNYALBAN124')
 
 # Pull the current obs for each station and append it to the running list
 for (id in stations) {
